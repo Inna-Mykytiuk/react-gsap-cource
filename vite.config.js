8cdeb64ef379fe608166f31583632fae3a38e510
Inna-Mykytiuk <innka-pinnka@ukr.net>
2024-10-01 17:03:13 +0300

    rename

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import { defineConfig } from "vite";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/react-gsap-cource",
+  base: "/react-gsap-course",
   resolve: {
     alias: {
       "@": path.resolve("./src"),
