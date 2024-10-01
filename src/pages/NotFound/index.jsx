c3590e8b97948da9264917eee1453272d1577b8e
Inna-Mykytiuk <innka-pinnka@ukr.net>
2024-10-01 17:00:20 +0300

    rename

--- src/pages/NotFound/index.jsx
+++ src/pages/NotFound/index.jsx
@@ -3,7 +3,7 @@ import { Link } from "react-router-dom";
 
 export default function NotFound() {
   return (
-    <div>
+    <div className="container">
       <p>Not Found</p>
       <Link to="/" className="text-xl font-semibold">
         Back Home
